f498be9e4710a70773cd357676fbb85c9c65c2fd
KristhDev <kristhdev@gmail.com>
2025-10-20 00:03:00 -0600

    docs(server): add system tag and upresponse schema
    Adds Swagger documentation for the 'System' tag and defines the 'UpResponse' component schema. This improves API documentation for system endpoints like '/up', enhancing discoverability and clarity for consumers.

--- src/presentation/server/controllers/base.controller.ts
+++ src/presentation/server/controllers/base.controller.ts
@@ -4,6 +4,29 @@ import { httpStatus } from '@application/constants';
 
 import { JsonResponseUtil } from '@server/utils';
 
+/**
+ * @swagger
+ * tags:
+ *   name: System
+ *   description: This tag is used to group all the system endpoints. Currently, the only system endpoint is /up.
+ *
+ * @swagger
+ * components:
+ *   responses:
+ *     UpResponse:
+ *       description: API is up and running
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 example: API is up and running
+ *               status:
+ *                 type: number
+ *                 example: 200
+ */
 export abstract class BaseController {
     protected readonly jsonResponse: JsonResponseUtil;
     protected readonly httpStatus = httpStatus;
