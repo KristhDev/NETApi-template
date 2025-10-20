5330071f263e763baaf0296dc58f1c0fcd4a632b
KristhDev <kristhdev@gmail.com>
2025-10-19 20:59:27 -0600

    feat(utils): add http response utility
    Introduces a utility class to standardize JSON success and error responses for HTTP requests.
    
    This centralizes the handling of various HTTP response types, including success, bad request, not found, and internal server errors, ensuring consistency across API endpoints. It leverages the `HttpError` domain entity for robust and structured error management.

---
+++ src/presentation/server/utils/json-response.util.ts
@@ -0,0 +1,83 @@
+import { Response } from 'express';
+
+/* Constants */
+import { httpStatus } from '@application/constants';
+
+/* Contracts */
+import { HttpError } from '@domain/errors';
+
+export interface JsonResponseData {
+    message?: string;
+    [key: string]: any;
+}
+
+export class JsonResponseUtil {
+
+    /**
+     * Sends a success response.
+     *
+     * @param {Response} res - The response object.
+     * @param {JsonResponseData} data - The response data.
+     * @return {void} This function does not return a value.
+     */
+    public success(res: Response, data: JsonResponseData): void {
+        data = Object.assign(data, { status: httpStatus.OK });
+        res = Object.assign(res, { response: data });
+
+        res.status(httpStatus.OK).json(data);
+    }
+
+    /**
+     * Sends an error response.
+     *
+     * @param {Response} res - The response object.
+     * @param {unknown} error - The error object.
+     * @return {void} This function does not return a value.
+     */
+    public error(res: Response, error: unknown): void {
+        let errorData: JsonResponseData = HttpError.internalServerError().toJSON();
+        let errorDataToLog: JsonResponseData = HttpError.internalServerError().toJSON();
+
+        if (error instanceof HttpError) {
+            errorData = error.toJSON();
+            errorDataToLog = error.toJSON();
+        }
+
+        res = Object.assign(res, { response: errorDataToLog });
+        res.status(errorData.status).json(errorData);
+    }
+
+    /**
+     * Sends a bad request response.
+     *
+     * @param {Response} res - The response object.
+     * @param {string} message - The error message.
+     * @return {void} This function does not return a value.
+     */
+    public badRequest(res: Response, message: string): void {
+        const badRequestError = HttpError.badRequest(message);
+        this.error(res, badRequestError);
+    }
+
+    /**
+     * Sends a not found response.
+     *
+     * @param {Response} res - The response object.
+     * @return {void} This function does not return a value.
+     */
+    public notFound(res: Response): void {
+        const notFoundError = HttpError.notFound();
+        this.error(res, notFoundError);
+    }
+
+    /**
+     * Sends an internal server error response.
+     *
+     * @param {Response} res - The response object.
+     * @return {void} This function does not return a value.
+     */
+    public internalServerError(res: Response): void {
+        const internalServerError = HttpError.internalServerError();
+        this.error(res, internalServerError);
+    }
+}
