0ff784a212ba43169dd2a947d3cb9ca84c8c93bd
KristhDev <kristhdev@gmail.com>
2025-10-19 21:14:00 -0600

    feat(server): initialize server middlewares and routes
    Integrates a request logging middleware to monitor incoming requests.
    Adds a `/up` endpoint for basic server health checks.
    Ensures all configured middlewares and routes are properly registered before the server starts listening.

--- src/presentation/server/server.ts
+++ src/presentation/server/server.ts
@@ -7,6 +7,12 @@ import { env } from '@config/env';
 /* Contracts */
 import { LoggerAdapterContract } from '@domain/contracts/adapters';
 
+/* Middlewares */
+import { LogRequestsMiddleware } from './middlewares';
+
+/* Controllers */
+import { UpController } from './controllers';
+
 class Server {
     private readonly port: number;
     private app: Application;
@@ -19,15 +25,23 @@ class Server {
     }
 
     private middlewares(): void {
+        const logRequestsMiddleware = new LogRequestsMiddleware(this.loggerAdapter);
+
         this.app.use(cors());
         this.app.use(express.json());
+        this.app.use((req, res, next) => logRequestsMiddleware.handle(req, res, next));
     }
 
     private routes(): void {
-        
+        const upController = new UpController();
+
+        this.app.get('/up', (req, res) => upController.handle(req, res));
     }
 
     public listen(): void {
+        this.middlewares();
+        this.routes();
+
         this.app.listen(this.port, () => {
             this.loggerAdapter.info(`Server running on port ${ this.port }`);
         });
