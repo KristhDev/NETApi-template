fc9657b755366081de94c21dd6d3739bb60e2239
KristhDev <kristhdev@gmail.com>
2025-10-19 21:09:08 -0600

    style(env): reorder environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,8 @@ import { get } from 'env-var';
 dotenv.config();
 
 export const env = {
-    APP_PORT: get('APP_PORT').required().asPortNumber(),
     APP_ENV: get('APP_ENV').required().asEnum([ 'local', 'production', 'testing' ]),
+    APP_PORT: get('APP_PORT').required().asPortNumber(),
 
     LOGS_DIR: get('LOGS_DIR').default('logs').asString(),
     LOGS_FILE_NAME: get('LOGS_FILE_NAME').default('application').asString(),
