10743e8d737f1020d847b1fc487c27c478623d80
KristhDev <kristhdev@gmail.com>
2025-10-19 23:58:53 -0600

    feat(config): add docs module alias
    This alias simplifies imports for documentation-related modules, enhancing code readability and maintainability.

--- paths.ts
+++ paths.ts
@@ -3,6 +3,7 @@ import moduleAlias from 'module-alias';
 moduleAlias.addAliases({
     '@application': __dirname + '/src/application',
     '@config': __dirname + '/src/config',
+    '@docs': __dirname + '/src/presentation/modules/docs',
     '@domain': __dirname + '/src/domain',
     '@infrastructure': __dirname + '/src/infrastructure',
     '@package': __dirname + '/package.json',
