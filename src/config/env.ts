9339a0d81d1bd4cd9a86f34d996c748402b6b008
KristhDev <kristhdev@gmail.com>
2025-10-19 23:59:07 -0600

    feat(config): add swagger contact and license env vars
    Adds required environment variables to configure Swagger's contact email, contact URL, and license URL dynamically.

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,5 +11,8 @@ export const env = {
     LOGS_DIR: get('LOGS_DIR').default('logs').asString(),
     LOGS_FILE_NAME: get('LOGS_FILE_NAME').default('application').asString(),
 
+    SWAGGER_CONTACT_EMAIL: get('SWAGGER_CONTACT_EMAIL').required().asString(),
+    SWAGGER_CONTACT_URL: get('SWAGGER_CONTACT_URL').required().asString(),
+    SWAGGER_LICENSE_URL: get('SWAGGER_LICENSE_URL').required().asString(),
     SWAGGER_OPEN_API_VERSION: get('SWAGGER_OPEN_API_VERSION').required().asString(),
 }
