e1f8b0ca6b689d5f12e9653e1cdddf2d69f45baa
KristhDev <kristhdev@gmail.com>
2025-10-20 12:13:33 -0600

    feat(di): add translation adapter to di config

--- src/config/di.ts
+++ src/config/di.ts
@@ -1,12 +1,18 @@
 import { env } from './env';
 
 /* Contracts */
-import { FileSystemAdapterContract, LoggerAdapterContract, YamlAdapterContract } from '@domain/contracts/adapters';
+import {
+    FileSystemAdapterContract,
+    LoggerAdapterContract,
+    TranslationAdapterContract,
+    YamlAdapterContract
+} from '@domain/contracts/adapters';
+
 import { OpenApiFacadeContract } from '@domain/contracts/facedes/docs';
 import { GenerateOpenApiJsonFileUseCaseContract, GenerateOpenApiYamlFileUseCaseContract } from '@domain/contracts/usecases/docs';
 
 /* Adapters */
-import { FileSystemAdapter, LoggerAdapter, YamlAdapter } from '@infrastructure/adapters';
+import { FileSystemAdapter, LoggerAdapter, TranslationAdapter, YamlAdapter } from '@infrastructure/adapters';
 
 /* Facades */
 import { OpenApiFacade } from '@infrastructure/facades/docs';
@@ -22,6 +28,7 @@ export const loggerAdapter: LoggerAdapterContract = new LoggerAdapter({
     writeLogsFilePerDay: true
 });
 
+export const translationAdapter: TranslationAdapterContract = new TranslationAdapter();
 export const yamlAdapter: YamlAdapterContract = new YamlAdapter();
 
 export const openApiFacade: OpenApiFacadeContract = new OpenApiFacade(fileSystemAdapter, loggerAdapter, yamlAdapter);
