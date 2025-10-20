9b24167ba7f9fac101374b66991928581727cd89
KristhDev <kristhdev@gmail.com>
2025-10-20 00:00:11 -0600

    feat(docs): add api documentation swagger ui
    Establishes routes to serve an interactive Swagger UI, allowing users to explore API endpoints directly in the browser. It also exposes the raw OpenAPI JSON specification for programmatic access.

---
+++ src/presentation/modules/docs/routes/router.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerSpec from '@config/swagger';
+
+import { env } from '@config/env';
+
+const docsRouter: Router = Router();
+
+docsRouter.use('/docs/swagger', swaggerUi.serve);
+
+docsRouter.get('/docs/swagger', swaggerUi.setup(swaggerSpec, {
+    explorer: true,
+    swaggerOptions: {
+        urls: [
+            {
+                name: `${ env.APP_NAME } OpenAPI`,
+                url: '/api/docs/openapi.json'
+            }
+        ]
+    }
+}));
+
+docsRouter.get('/docs/openapi.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.json(swaggerSpec);
+});
+
+export default docsRouter;
