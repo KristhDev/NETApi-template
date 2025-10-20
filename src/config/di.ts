a989e325a2ba3ba92606a59757ec0a0b5647891e
KristhDev <kristhdev@gmail.com>
2025-10-20 08:26:54 -0600

    feat(di): add openapi generation dependencies
    Registers the FileSystemAdapter, OpenApiFacade, and GenerateOpenApiJsonFileUseCase to enable the generation of OpenAPI JSON documentation files.

--- src/config/di.ts
+++ src/config/di.ts
@@ -1,13 +1,27 @@
 import { env } from './env';
 
 /* Contracts */
-import { LoggerAdapterContract } from '@domain/contracts/adapters';
+import { FileSystemAdapterContract, LoggerAdapterContract } from '@domain/contracts/adapters';
+import { OpenApiFacadeContract } from '@domain/contracts/facedes/docs';
+import { GenerateOpenApiJsonFileUseCaseContract } from '@domain/contracts/usecases/docs';
 
 /* Adapters */
-import { LoggerAdapter } from '@infrastructure/adapters';
+import { FileSystemAdapter, LoggerAdapter } from '@infrastructure/adapters';
+
+/* Facades */
+import { OpenApiFacade } from '@infrastructure/facades/docs';
+
+/* Use Cases */
+import { GenerateOpenApiJsonFileUseCase } from '@application/usecases/docs';
+
+export const fileSystemAdapter: FileSystemAdapterContract = new FileSystemAdapter();
 
 export const loggerAdapter: LoggerAdapterContract = new LoggerAdapter({
     logFileName: env.LOGS_FILE_NAME,
     logsDir: env.LOGS_DIR,
     writeLogsFilePerDay: true
 });
+
+export const openApiFacade: OpenApiFacadeContract = new OpenApiFacade(fileSystemAdapter, loggerAdapter);
+
+export const generateOpenApiJsonFileUseCase: GenerateOpenApiJsonFileUseCaseContract = new GenerateOpenApiJsonFileUseCase(openApiFacade);
