33b8220660078d9cf73dc437d653e2cbaaa1b06b
KristhDev <kristhdev@gmail.com>
2025-10-20 12:09:44 -0600

    feat(i18n): configure initial internationalization
    Configures i18next for multilingual support, setting up resource loading, default language, and interpolation. Provides type definitions for translation keys to enhance type safety and improve developer experience.

---
+++ src/config/i18n.ts
@@ -0,0 +1,38 @@
+import i18next from 'i18next';
+
+import en from '@locales/en.json';
+import es from '@locales/es.json';
+
+export const resources = {
+    en: { translation: en },
+    es: { translation: es }
+}
+
+export const defaultNS = 'translation';
+
+type Join<K, P> = K extends string | number
+    ? P extends string | number
+    ? `${K}.${P}`
+    : K
+    : never;
+
+type ObjectKeys<T> = T extends object
+    ? {
+        [K in keyof T]: T[K] extends object
+        ? Join<K, ObjectKeys<T[K]>>
+        : K;
+    }[keyof T]
+    : never;
+
+export type Resources = typeof resources;
+export type TranslationKeys = ObjectKeys<Resources["es"]["translation"]>;
+
+i18next.init({
+    defaultNS,
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+    lng: 'en',
+    resources
+});
+
+export { i18next }
