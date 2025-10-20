3062924356371579c132c7b46477ca5108565c9a
KristhDev <kristhdev@gmail.com>
2025-10-19 20:02:29 -0600

    feat(di): add logger dependency injection
    Sets up the logger adapter instance with environment-specific configuration, centralizing its creation for consistent use.

---
+++ src/config/di.ts
@@ -0,0 +1,10 @@
+import { env } from './env';
+
+import { LoggerAdapterContract } from '@domain/contracts/adapters';
+
+import { LoggerAdapter } from '@infrastructure/adapters';
+
+export const loggerAdapter: LoggerAdapterContract = new LoggerAdapter({
+    logFileName: env.LOGS_FILE_NAME,
+    logsDir: env.LOGS_DIR
+});
