e1d8eeb40bb999c1975d3263f9fe88651c67433d
KristhDev <kristhdev@gmail.com>
2025-10-19 21:08:55 -0600

    feat(di): enable daily log file rotation
    Ensures log files are rotated daily to improve log management and prevent excessively large log files.

--- src/config/di.ts
+++ src/config/di.ts
@@ -6,5 +6,6 @@ import { LoggerAdapter } from '@infrastructure/adapters';
 
 export const loggerAdapter: LoggerAdapterContract = new LoggerAdapter({
     logFileName: env.LOGS_FILE_NAME,
-    logsDir: env.LOGS_DIR
+    logsDir: env.LOGS_DIR,
+    writeLogsFilePerDay: true
 });
