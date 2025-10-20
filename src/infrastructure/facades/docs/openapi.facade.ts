810e343a49eb30bbbd72da0cb4e518e01d34109c
KristhDev <kristhdev@gmail.com>
2025-10-20 08:22:34 -0600

    feat(facades): add openapi json file generation
    Encapsulates the logic for generating the OpenAPI JSON specification file. It leverages file system and logger adapters, and uses environment configuration for output path and name, ensuring consistent API documentation generation.

---
+++ src/infrastructure/facades/docs/openapi.facade.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+
+/* Config */
+import { env } from '@config/env';
+import swaggerSpec from '@config/swagger';
+
+/* Contracts */
+import { FileSystemAdapterContract, LoggerAdapterContract } from '@domain/contracts/adapters';
+import { OpenApiFacadeContract } from '@domain/contracts/facedes/docs';
+
+/* Errors */
+import { BaseError } from '@domain/errors';
+
+export class OpenApiFacade implements OpenApiFacadeContract {
+    public constructor(
+        private readonly fileSystemAdapter: FileSystemAdapterContract,
+        private readonly loggerAdapter: LoggerAdapterContract,
+    ) {}
+
+    /**
+     * Generates an OpenAPI JSON file.
+     *
+     * @returns {Promise<void>} A promise that resolves when the file is generated.
+     */
+    public async generateOpenApiJsonFile(): Promise<void> {
+        try {
+            this.loggerAdapter.info('Generating OpenAPI JSON file...');
+
+            this.loggerAdapter.info('Creating directory if it does not exist...');
+            const dirPath = `${ process.cwd() }/${ env.SWAGGER_OPEN_API_FILE_DIR }`;
+            await this.fileSystemAdapter.mkdirIfNotExists(dirPath);
+
+            this.loggerAdapter.info('Writing file...');
+            const filePath = path.join(dirPath, `${ env.SWAGGER_OPEN_API_FILE_NAME }.json`);
+            await this.fileSystemAdapter.writeFile(filePath, JSON.stringify(swaggerSpec, null, 2));
+
+            this.loggerAdapter.info('OpenAPI JSON file generated successfully');
+        } 
+        catch (error) {
+            const errorData = (error as BaseError<{}>).toJSON(); 
+            this.loggerAdapter.error('Failed to generate OpenAPI JSON file', errorData);
+        }
+    }
+}
