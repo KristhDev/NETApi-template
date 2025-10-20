b32f8d9678a4361403826c977448b0366c6d1a38
KristhDev <kristhdev@gmail.com>
2025-10-20 08:14:59 -0600

    feat(errors): add new file system error type
    Introduces a dedicated error class for file system operations, `FileSystemError`.
    This enhances error handling by providing a specific type for issues related to file system interactions.

---
+++ src/domain/errors/file-system.error.ts
@@ -0,0 +1,20 @@
+import { BaseError } from './base.error';
+
+interface FileSystemErrorJson {
+    error: string;
+    message: string;
+}
+
+export class FileSystemError extends BaseError<FileSystemErrorJson> {
+    constructor(message: string) {
+        super(message);
+        this.name = 'FileSystemError';
+    }
+
+    public toJSON(): FileSystemErrorJson {
+        return {
+            error: this.name,
+            message: this.message
+        }
+    }
+}
--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,2 +1,3 @@
 export * from './base.error';
 export * from './http.error';
+export * from './file-system.error';
